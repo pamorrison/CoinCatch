57b92b9c7184cba768d6d18d4787cf38362a157b
Paul Morrison <pamorrison@email.wm.edu>
2019-02-02 18:32:56 -0500

    Initial Commit

---
+++ CoinCatch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CoinCatch
+//
+//  Created by Paul Morrison on 2/2/19.
+//  Copyright © 2019 Paul Morrison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
